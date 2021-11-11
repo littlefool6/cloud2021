080f8da66a645fca7a3ee7482472e42e7061fd11
littlefool <guozelongha123@163.com>
2021-11-11 09:46:22 +0800

    feat：add cloud-eureka-server7002

--- cloud-eureka-server7002/src/main/java/com/klfd/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/klfd/springcloud/EurekaMain7002.java
@@ -2,19 +2,19 @@ package com.klfd.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @ClassName OrderMain80
+ * @ClassName EurekaMain
  * @Description TODO
  * @Author zlguo
- * @Date 2021/10/10
+ * @Date 2021/11/9
 **/
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7002.class, args);
     }
 }
