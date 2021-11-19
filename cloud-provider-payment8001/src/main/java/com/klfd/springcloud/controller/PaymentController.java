d18037379e02b9c86ec0aa493fa5d747720eac78
littlefool <guozelongha123@163.com>
2021-11-19 15:25:46 +0800

    feat：add .ignore file

--- cloud-provider-payment8001/src/main/java/com/klfd/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/klfd/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.klfd.springcloud.entities.Payment;
 import com.klfd.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @ClassName PaymentController
@@ -23,8 +26,24 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @Value("${server.port}")
     private String serverPort;
+
+    @GetMapping(value = "discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("****service：" + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
+    }
 
     @PostMapping(value = "create")
     public CommonResult create(@RequestBody Payment payment) {
