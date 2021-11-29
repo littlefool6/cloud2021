67b28968d7fe80b1fc586d92e98e3b716218d323
littlefool6 <guozelongha123@163.com>
2021-11-29 17:37:52 +0800

    feat：add zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/klfd/springcloud/PaymentMain8004.java
@@ -0,0 +1,20 @@
+package com.klfd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName PaymentMain8004
+ * @Description TODO
+ * @Author zlguo
+ * @Date 2021/11/29
+**/
+@SpringBootApplication
+@EnableDiscoveryClient // 该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/klfd/springcloud/controller/PaymentController.java
@@ -0,0 +1,12 @@
+package com.klfd.springcloud.controller;
+
+/**
+ * @ClassName PaymentController
+ * @Description TODO
+ * @Author zlguo
+ * @Date 2021/11/29
+**/
+public class PaymentController {
+
+
+}
