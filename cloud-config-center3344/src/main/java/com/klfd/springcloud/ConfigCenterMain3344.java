2bfb20c0b6d380d2ff89de438ecc7a052c955c3f
littlefool <guozelongha123@163.com>
2022-03-07 22:41:54 +0800

    feat：add spring-cloud config

---
+++ cloud-config-center3344/src/main/java/com/klfd/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.klfd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @ClassName ConfigCenterMain3344
+ * @Description TODO
+ * @Author zlguo
+ * @Date 2022/3/5
+**/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
