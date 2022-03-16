c6d03e49deac76562dadfdebf7c065ba7c9922c8
littlefool <guozelongha123@163.com>
2022-03-16 23:06:20 +0800

    feat：add SpringCloud Bus

---
+++ cloud-config-client-3366/src/main/java/com/klfd/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.klfd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName ConfigClientMain3366
+ * @Description TODO
+ * @Author zlguo
+ * @Date 2022/3/16
+**/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
